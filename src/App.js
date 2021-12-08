e255034953640b34c7af4b9136839bf904b96db0
Aj-github-cmd <sahooamlanjyoti99@gmail.com>
2021-12-09 02:53:26 +0530

    mark 8 project-emoji zoo

--- src/App.js
+++ src/App.js
@@ -1,23 +1,46 @@
-import logo from './logo.svg';
+import React,{useState} from 'react';
 import './App.css';
-
+import emojjiDb from './api/EmojiDb';
 function App() {
+  const emojiList = Object.keys(emojjiDb)
+  const [emoji, setEmoji] = useState("")
+  const [name, setName] = useState("")
+  const clickHandler = (elem)=>{
+   setName(emojjiDb[elem])
+  }
+  const inputHandler =()=>{
+  const emojiName = emojjiDb[emoji]
+  if(emoji !== ""){
+    if(emoji in emojjiDb){
+      setName(emojiName)
+    }else{
+      setName('Emoji not found')
+    }
+  }else{
+    alert('Please enter a emoji')
+  }
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="heading"><h3>WELCOME TO EMOJI ZOO</h3></div>
+    <div className="inputarea">
+      <input type="text" placeholder="Enter an animal emoji"
+       onChange={(e)=>{setEmoji(e.target.value)}}/>
+      <button onClick={inputHandler}>Check</button>
+    </div>
+   <div className="emojiname"><h3>{name}</h3></div>
+    <div className="emojiarea">
+    {
+    emojiList.map((elem,key)=>{
+       return(
+         <p onClick={()=>{clickHandler(elem)}}
+         key={key}
+         >{elem}</p>
+       )
+     })
+    }
+    </div>
+
     </div>
   );
 }
---
+++ src/api/EmojiDb.js
@@ -0,0 +1,19 @@
+const emojjiDb = {
+
+    "🐺":"Wolf",
+    "🦄":"Unicorn",
+    "🐼":"Panda",
+    "🐨":"Koala",
+    "🦍":"Gorilla",
+    "🐘":"Elephant",
+    "🦒":"Giraffe",
+    "🦁":"Lion",
+    "🦓":"Zebra",
+    "🦏":"Rhinoceros",
+    "🦝":"Raccoon",
+    "🐯":"Tiger",
+    "🦛":"Hippopotamus",
+    "🐹":"Hamster",
+    "🦉":"Owl"
+}
+export default emojjiDb;
